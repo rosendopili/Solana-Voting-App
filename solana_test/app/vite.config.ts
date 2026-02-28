1b874558f355adb931dcc7ab5d8d9bb3ed7979ac
Rosendo Pili <rosendopili@gmail.com>
2026-02-27 22:44:24 -0500

    fix asset paths for GitHub pages

--- solana_test/app/vite.config.ts
+++ solana_test/app/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Solana-Voting-App-/',
+  base: './',
   plugins: [react()],
   define: {
     global: 'globalThis',
